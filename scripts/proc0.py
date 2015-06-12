991e63bf5278470252f3ea716a717783c3afecc5
astrofle <salas.mz@gmail.com>
2015-06-12 19:35:05 +0200

    Changes to proc0.py

--- scripts/proc0.py
+++ scripts/proc0.py
@@ -2,25 +2,44 @@
 
 import sys
 import argparse
+import glob
 import numpy as np
 from astropy.table import Table
 from astropy.io import ascii
+from crrlpy import crrls
 
 def main(spec, out):
     """
     """
     
-    data = np.loadtxt(spec, comments='#')
+    specs = glob.glob(spec)
     
-    freq = data[:,0]/1e6
-    tb = data[:,1]
+    # Sort the SBs
+    crrls.natural_sort(specs)
     
-    # write the processed spectrum
-    tbtable = Table([freq, tb], 
-                    names=['FREQ MHz',
-                           'Tb Jy/BEAM'])
+    for s in specs:
+        
+        # Determine the subband name
+        try:
+            sb = re.findall('SB\d+', s)[0]
+        except IndexError:
+            print "Could not find SB number."
+            print "Will use SB???"
+            sb = 'SB???'
+        
+        data = np.loadtxt(s, comments='#')
+        
+        freq = data[:,0]/1e6
+        tb = data[:,1]
+        
+        # write the processed spectrum
+        tbtable = Table([freq, tb], 
+                        names=['FREQ MHz',
+                            'Tb Jy/BEAM'])
+                        
+        out = '{0}_{1}.ascii'.format(basename, sb)
 
-    ascii.write(tbtable, out, format='commented_header')
+        ascii.write(tbtable, out, format='commented_header')
     
 if __name__ == '__main__':
     
@@ -28,7 +47,7 @@ if __name__ == '__main__':
     parser.add_argument('spec', 
                         help="Spectrum to process.")
     parser.add_argument('-o', '--outspec', 
-                        help="Output spectrum name.", 
+                        help="Output spectrum base name.", 
                         type=str, dest='out', required=True)
     args = parser.parse_args()
     
