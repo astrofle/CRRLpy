09568d3b7292c3eba40e20be4e515ff7e529e6f7
astrofle <salas.mz@gmail.com>
2015-09-04 13:00:40 +0200

    Started restructuring the module into submodules.

---
+++ crrlpy/utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def sci_notation(number, sig_fig=2):
+    """
+    Converts a number to scientific notation keeping sig_fig signitifcant figures.
+    """
+    
+    ret_string = "{0:.{1:d}e}".format(number, sig_fig)
+    a,b = ret_string.split("e")
+    b = int(b) #removed leading "+" and strips leading zeros too.
+    
+    return r"{0}\times10^{{{1}}}".format(a, b)
