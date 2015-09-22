da9671f862b8721599f5f6032f2560876311040b
astrofle <salas.mz@gmail.com>
2015-09-22 16:25:38 +0200

    added option to add continuum to synthetic spetra

--- scripts/make_spec.py
+++ scripts/make_spec.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 
-import numpy as np
+
 import argparse
+import os
+import matplotlib as mpl
+havedisplay = "DISPLAY" in os.environ
+if not havedisplay:
+    mpl.use('Agg')
+
+import numpy as np
+import pylab as plt
+
 from crrlpy import crrls
 from crrlpy.models import rrlmod
 from crrlpy import synthspec as synth
-import pylab as plt
+
+def power_law(freq, Tc, nu):
+    """
+    """
+    
+    return Tc*np.power(freq/100., nu)
 
 def make_spec(spec, fi, bw, n, rms, v0, transitions, bandpass, baseline, order,
-              Te, ne, Tr, W, dD, EM, 
+              Te, ne, Tr, W, dD, EM, cont, Tc, nu, plot, plot_out,
               n_max=1500, verbose=False):
     """
     Generates a synthetic spectrum given an initial frequency a bandwidth and number of channels.
@@ -42,30 +56,44 @@ def make_spec(spec, fi, bw, n, rms, v0, transitions, bandpass, baseline, order,
     
     z = v0/3e5
     for i,trans in enumerate(transitions.split(',')):
-        
-        n_l, f_l = crrls.find_lines_sb(freq, trans, z)
-        
-        # Generate the line properties as a function of n
-        dL_r = crrls.radiation_broad_salgado(n_l, W, Tr)/1e6
-        dL_p = crrls.pressure_broad_salgado(n_l, Te, ne)/1e6
-        dL = dL_r + dL_p
-        n_itau, a_itau = rrlmod.itau(rrlmod.val2str(Te), 
-                                     ne, 
-                                     trans[2:], n_max=n_max, 
-                                     other='case_diffuse_{0}'.format(rrlmod.val2str(Tr)),
-                                     verbose=verbose)
-    
-        dD_f = crrls.dv2df(f_l, dD*1e3)
-        
-        for j,f in enumerate(f_l):
-            itau = a_itau[np.where(n_itau==n_l[j])[0]][0]/1e6
-            line = crrls.Voigt(freq, dD_f[j]/2., dL[j]/2., f, itau*EM)
-            if verbose:
-                print "Line properties:"
-                print("f: {0}, A: {1}, dD: {2}, dD_f/2: {3}, " \
-                    "dL/2: {4}".format(f, itau*EM, dD, dD_f[j]/2., dL[j]/2.))
-            tau_n += line
+        if trans != '':
+            n_l, f_l = crrls.find_lines_sb(freq, trans, z)
             
+            # Generate the line properties as a function of n
+            dL_r = crrls.radiation_broad_salgado(n_l, W, Tr)/1e6
+            dL_p = crrls.pressure_broad_salgado(n_l, Te, ne)/1e6
+            dL = dL_r + dL_p
+            n_itau, a_itau = rrlmod.itau(rrlmod.val2str(Te), 
+                                        ne, 
+                                        trans[2:], n_max=n_max, 
+                                        other='case_diffuse_{0}'.format(rrlmod.val2str(Tr)),
+                                        verbose=verbose)
+        
+            dD_f = crrls.dv2df(f_l, dD*1e3)
+            
+            for j,f in enumerate(f_l):
+                itau = a_itau[np.where(n_itau==n_l[j])[0]][0]/1e6
+                line = crrls.Voigt(freq, dD_f[j]/2., dL[j]/2., f, itau*EM)
+                if verbose:
+                    print "Line properties:"
+                    print("f: {0}, A: {1}, dD: {2}, dD_f/2: {3}, " \
+                        "dL/2: {4}".format(f, itau*EM, dD, dD_f[j]/2., dL[j]/2.))
+                tau_n += line
+            
+    if cont:
+        tau_n = (1 + tau_n)*power_law(freq, Tc, nu)
+        
+    if plot:
+        fig = plt.figure(frameon=False)
+        ax = fig.add_subplot(1, 1, 1)
+        ax.plot(freq, tau_n, 'k-', label='generated spectrum')
+        ax.plot(freq, power_law(freq, Tc, nu), 'r-', label='continuum')
+        plt.legend(loc=0, numpoints=1, frameon=False)
+        plt.savefig('{0}'.format(plot_out), 
+                bbox_inches='tight', pad_inches=0.3)
+        plt.close()
+        
+        
     np.savetxt(spec, np.c_[freq, tau_n])
     
     
@@ -110,8 +138,20 @@ if __name__ == '__main__':
                         help="Add a baseline offset to the spectrum?")
     parser.add_argument('--order', type=int, default=None,
                         help="Baseline order to add to the spectrum.")
+    parser.add_argument('--cont', action='store_true',
+                        help="Add continuum to the spectra?.\n" \
+                             "Assumes a power law spectrum with Tc at 100 MHz.")
+    parser.add_argument('--Tc', type=float,
+                        help="Power law continuum intensity at 100 MHz. (float)")
+    parser.add_argument('--plawexp', type=float, default=-2.6,
+                        help="Continuum power law exponent.")
+    parser.add_argument('--plot', action='store_true',
+                        help="Plot the generated spectrum?")
+    parser.add_argument('--plot_out', type=str,
+                        help="Plot name. (string)")
     args = parser.parse_args()
     
     make_spec(args.spec, args.fi, args.bw, args.nchan, args.rms, args.v0, 
               args.transitions, args.baseline, args.order,
-              args.Te, args.ne, args.Tr, args.w, args.doppler, args.EM)
+              args.Te, args.ne, args.Tr, args.w, args.doppler, args.EM, 
+              args.cont, args.Tc, args.plawexp, args.plot, args.plot_out)
--- scripts/remove_cont.py
+++ scripts/remove_cont.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
-from lmfit import Model
-import numpy as np
 import sys
 import argparse
+import os
+import matplotlib as mpl
+havedisplay = "DISPLAY" in os.environ
+if not havedisplay:
+    mpl.use('Agg')
+
+from lmfit import Model
+from crrlpy import crrls
+
+import pylab as plt
+import numpy as np
 
 def linear(x, a, b):
     """
@@ -21,7 +30,22 @@ def nan_mask(freq, tb):
     
     return mask
 
-def remove_baseline(freq, tb, model, p0, mask):
+def fit_baseline(freq, tb, model, p0):
+    """
+    """
+    mod = Model(model)
+    params = mod.make_params()
+    if len(p0) != len(params):
+        print "Insuficient starting parameter values."
+        return 0
+    else:
+        for param in params:
+            params[param].set(value=p0[param])
+
+    return mod.fit(tb, x=freq, params=params)
+    
+
+def remove_baseline(freq, tb, model, p0):
     """
     Divide tb by given a model
     and starting parameters p0.
@@ -37,13 +61,12 @@ def remove_baseline(freq, tb, model, p0, mask):
         for param in params:
             params[param].set(value=p0[param])
 
-    fit = mod.fit(tb[~mask], x=freq[~mask], params=params)
-    #best_fit = mod.eval(x=freq, amp=10, cen=6.2, wid=0.75)
+    fit = mod.fit(tb, x=freq, params=params)
     tbcsub = tb/fit.eval(x=freq) - 1
     
     return tbcsub
 
-def main(spec_in, spec_out, edge, xcol, ycol):
+def main(spec_in, spec_out, edge, xcol, ycol, transitions, z, dv, plot, plot_out):
     """ main """
     
     # Load the data
@@ -54,6 +77,22 @@ def main(spec_in, spec_out, edge, xcol, ycol):
     # Get a mask for NaN values
     mask = nan_mask(freq, tb)
     
+    # Remove NaNs and invalid values
+    x = freq[~mask]
+    y = tb[~mask]
+    
+    # Blank lines
+    trans = transitions.split(',')
+    bf = []
+    for o,t in enumerate(trans):
+        n, f = crrls.find_lines_sb(x, t, z)
+        bf.append(list(f))
+    if len(bf) > 0:
+        bf = np.array(list(_f for _bf in bf for _f in _bf))
+        x_lf, y_lf = crrls.blank_lines2(x, y, bf, dv)
+    else:
+        x_lf, y_lf = x,y
+    
     # Set the initial parameters to fit the continuum
     a = (tb[~mask][-1-edge] - tb[~mask][0+edge])/\
         (freq[~mask][-1-edge] - freq[~mask][0+edge])
@@ -61,14 +100,28 @@ def main(spec_in, spec_out, edge, xcol, ycol):
     p0 = {'a':a, 'b':b}
     
     # Remove the continuum
-    tbcsub = remove_baseline(freq, tb, linear, p0, mask)
+    #tbcsub = remove_baseline(freq[~mask], tb[~mask], linear, p0)
+    baseline = fit_baseline(x_lf, y_lf, linear, p0)
+    tbcsub = tb/baseline.eval(x=freq) - 1
+    
+    if plot:
+        fig = plt.figure(frameon=False)
+        ax = fig.add_subplot(1, 1, 1)
+        ax.plot(freq, tb, 'k-', label='in')
+        ax.plot(freq, baseline.eval(x=freq), 'r-', label='continuum')
+        ax.plot(x_lf, y_lf, 'b-', label='line free spectra')
+        plt.legend(loc=0, numpoints=1, frameon=False)
+        plt.savefig('{0}'.format(plot_out), 
+                bbox_inches='tight', pad_inches=0.3)
+        plt.close()
     
     # Save the continuum subtracted spectrum
     np.savetxt(spec_out, np.c_[freq, tbcsub])
     
 if __name__ == '__main__':
     
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('spec', help="Spectrum to remove continuum from.")
     parser.add_argument('out', help="Output spectrum without continuum.")
     parser.add_argument('-e', '--edge', help="How many edge channels \
@@ -77,6 +130,21 @@ if __name__ == '__main__':
                         help="Column with x axis values. Default: 0")
     parser.add_argument('--y_col', type=int, default=1,
                         help="Column with y axis values. Default: 1")
+    parser.add_argument('-t', '--transitions', type=str, default='CIalpha',
+                        help="Transitions to blank in the spectra. (string)\n" \
+                             "E.g., CIalpha,CI13beta,HIalpha\n" \
+                             "Default: CIalpha.")
+    parser.add_argument('--z', type=float, default=0.0, dest='z',
+                        help="Redshift to apply to the transition rest frequency. (float)\n" \
+                             "Default: 0")
+    parser.add_argument('-d', '--dv', type=float, default=50.,
+                        help="Velocity range to blank around each transition \n" \
+                             "in km/s. (float, km/s)\n" \
+                             "Default: 50")
+    parser.add_argument('--plot', action='store_true',
+                        help="Plot the generated spectrum?")
+    parser.add_argument('--plot_out', type=str,
+                        help="Plot name. (string)")
     args = parser.parse_args()
     
     spec = args.spec
@@ -84,5 +152,10 @@ if __name__ == '__main__':
     edge = args.edge
     xcol = args.x_col
     ycol = args.y_col
+    transitions = args.transitions
+    z = args.z
+    dv = args.dv
+    plot = args.plot
+    plot_out = args.plot_out
     
-    main(spec, out, edge, xcol, ycol)
+    main(spec, out, edge, xcol, ycol, transitions, z, dv, plot, plot_out)
